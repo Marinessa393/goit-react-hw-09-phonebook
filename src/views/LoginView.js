c6225f0a91be4685bd3e5001958ef99e2306a455
Marinessa393 <futurism1303@gmail.com>
2021-05-05 19:42:14 +0300

    feat: change styles for login form and register form

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,21 +1,78 @@
 import {
   Avatar,
+  Box,
   Button,
+  CssBaseline,
+  Grid,
   IconButton,
   InputAdornment,
+  Link,
+  makeStyles,
+  Paper,
   TextField,
+  Typography,
 } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import operations from '../redux/auth/auth-operations';
-import LockIcon from '@material-ui/icons/Lock';
-import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { LockOutlined, Visibility, VisibilityOff } from '@material-ui/icons';
 import { useCallback, useState } from 'react';
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="secondary" href="https://github.com/Marinessa393">
+        Marinessa
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: 'calc(100vh - 50px)',
+  },
+  image: {
+    backgroundImage:
+      'url(https://source.unsplash.com/collection/71191721/1600x900)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.light,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 function LoginView() {
+  const classes = useStyles();
+
+  // state
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+
+  // Dispatch
   const dispatch = useDispatch();
   const onLogin = useCallback(
     ({ email, password }) => dispatch(operations.logIn({ email, password })),
@@ -38,74 +95,89 @@ function LoginView() {
 
   return (
     <>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          handleSumbit({ email, password });
-        }}
-        className="login_form"
-      >
-        <Avatar className="formAvatar">
-          <LockIcon color="action" />
-        </Avatar>
-        <TextField
-          id="email"
-          name="email"
-          value={email}
-          type="email"
-          label="Email Address"
-          margin="normal"
-          required
-          fullWidth
-          autoComplete="email"
-          autoFocus
-          onChange={event => setEmail(event.target.value)}
-          color="secondary"
-        />
-        <TextField
-          id="password-log"
-          name="password"
-          value={password}
-          type={showPassword ? 'text' : 'password'}
-          label="Password"
-          margin="normal"
-          required
-          fullWidth
-          onChange={event => setPassword(event.target.value)}
-          color="secondary"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          className="btn"
-          color="secondary"
-          fullWidth
-        >
-          Login
-        </Button>
-
-        <NavLink
-          to="/register"
-          exact
-          className="authRouterLink"
-          activeClassName="authRouterLink_active"
-        >
-          Don't have an account? Sign Up
-        </NavLink>
-      </form>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <form
+              className={classes.form}
+              onSubmit={e => {
+                e.preventDefault();
+                handleSumbit({ email, password });
+              }}
+            >
+              <TextField
+                id="email-log"
+                name="email"
+                value={email}
+                type="email"
+                label="Email Address"
+                required
+                fullWidth
+                autoComplete="email"
+                autoFocus
+                variant="outlined"
+                margin="normal"
+                color="secondary"
+                onChange={event => setEmail(event.target.value)}
+              />
+              <TextField
+                id="password-log"
+                name="password"
+                value={password}
+                type={showPassword ? 'text' : 'password'}
+                label="Password"
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                color="secondary"
+                autoComplete="current-password"
+                onChange={event => setPassword(event.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                className={classes.submit}
+              >
+                Login
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="/register" variant="body2" color="primary">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
     </>
   );
 }
--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,17 +1,72 @@
 import {
   Avatar,
+  Box,
   Button,
+  CssBaseline,
+  Grid,
   IconButton,
   InputAdornment,
+  makeStyles,
+  Paper,
+  Link,
   TextField,
+  Typography,
 } from '@material-ui/core';
-import { Visibility, VisibilityOff } from '@material-ui/icons';
-import LockIcon from '@material-ui/icons/Lock';
+import { LockOutlined, Visibility, VisibilityOff } from '@material-ui/icons';
 import { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import operations from '../redux/auth/auth-operations';
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="primary" href="https://github.com/Marinessa393">
+        Marinessa
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: 'calc(100vh - 50px)',
+  },
+  image: {
+    backgroundImage:
+      'url(https://source.unsplash.com/collection/71191721/1600x900)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.light,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 function RegisterView() {
+  const classes = useStyles();
+
   // state
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -46,81 +101,174 @@ function RegisterView() {
 
   return (
     <>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          handleSubmit({ name, email, password });
-        }}
-        className="login_form"
-      >
-        <Avatar className="formAvatar">
-          <LockIcon color="action" />
-        </Avatar>
-
-        <TextField
-          id="name-reg"
-          name="name"
-          value={name}
-          type="name"
-          label="Name"
-          margin="normal"
-          required
-          fullWidth
-          autoComplete="name"
-          autoFocus
-          onChange={event => setName(event.target.value)}
-          color="primary"
-        />
-        <TextField
-          id="email-reg"
-          name="email"
-          value={email}
-          type="email"
-          label="Email Address"
-          margin="normal"
-          required
-          fullWidth
-          autoComplete="email"
-          onChange={event => setEmail(event.target.value)}
-          color="primary"
-        />
-        <TextField
-          id="password-reg"
-          name="password"
-          value={password}
-          type={showPassword ? 'text' : 'password'}
-          label="Password"
-          margin="normal"
-          required
-          fullWidth
-          onChange={event => setPassword(event.target.value)}
-          color="primary"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-
-        <Button
-          variant="contained"
-          type="submit"
-          className="btn"
-          color="primary"
-          fullWidth
-        >
-          Join
-        </Button>
-      </form>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+            <form
+              className={classes.form}
+              onSubmit={e => {
+                e.preventDefault();
+                handleSubmit({ name, email, password });
+              }}
+            >
+              <TextField
+                id="name-reg"
+                name="name"
+                value={name}
+                type="name"
+                label="Username"
+                required
+                fullWidth
+                autoComplete="name"
+                autoFocus
+                variant="outlined"
+                margin="normal"
+                color="primary"
+                onChange={event => setName(event.target.value)}
+              />
+              <TextField
+                id="email-reg"
+                name="email"
+                value={email}
+                type="email"
+                label="Email Address"
+                required
+                fullWidth
+                autoComplete="email"
+                variant="outlined"
+                margin="normal"
+                color="primary"
+                onChange={event => setEmail(event.target.value)}
+              />
+              <TextField
+                id="password-reg"
+                name="password"
+                value={password}
+                type={showPassword ? 'text' : 'password'}
+                label="Password"
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                color="primary"
+                autoComplete="current-password"
+                onChange={event => setPassword(event.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Join
+              </Button>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
     </>
+
+    // <>
+    //   <form
+    //     onSubmit={e => {
+    //       e.preventDefault();
+    //       handleSubmit({ name, email, password });
+    //     }}
+    //     className="login_form"
+    //   >
+    //     <Avatar className="formAvatar">
+    //       <LockIcon color="action" />
+    //     </Avatar>
+
+    //     <TextField
+    //       id="name-reg"
+    //       name="name"
+    //       value={name}
+    //       type="name"
+    //       label="Name"
+    //       margin="normal"
+    //       required
+    //       fullWidth
+    //       autoComplete="name"
+    //       autoFocus
+    //       onChange={event => setName(event.target.value)}
+    //       color="primary"
+    //     />
+    //     <TextField
+    //       id="email-reg"
+    //       name="email"
+    //       value={email}
+    //       type="email"
+    //       label="Email Address"
+    //       margin="normal"
+    //       required
+    //       fullWidth
+    //       autoComplete="email"
+    //       onChange={event => setEmail(event.target.value)}
+    //       color="primary"
+    //     />
+    //     <TextField
+    //       id="password-reg"
+    //       name="password"
+    //       value={password}
+    //       type={showPassword ? 'text' : 'password'}
+    //       label="Password"
+    //       margin="normal"
+    //       required
+    //       fullWidth
+    //       onChange={event => setPassword(event.target.value)}
+    //       color="primary"
+    //       InputProps={{
+    //         endAdornment: (
+    //           <InputAdornment position="end">
+    //             <IconButton
+    //               aria-label="toggle password visibility"
+    //               onClick={handleClickShowPassword}
+    //               onMouseDown={handleMouseDownPassword}
+    //             >
+    //               {showPassword ? <Visibility /> : <VisibilityOff />}
+    //             </IconButton>
+    //           </InputAdornment>
+    //         ),
+    //       }}
+    //     />
+
+    //     <Button
+    //       variant="contained"
+    //       type="submit"
+    //       className="btn"
+    //       color="primary"
+    //       fullWidth
+    //     >
+    //       Join
+    //     </Button>
+    //   </form>
+    // </>
   );
 }
 
