37540cf817681b0dc14274eec68daeb180a134e9
Marinessa393 <futurism1303@gmail.com>
2021-05-05 23:39:18 +0300

    fix styles

--- src/components/Navigation/AppBar/AppBar.js
+++ src/components/Navigation/AppBar/AppBar.js
@@ -1,3 +1,4 @@
+import { Toolbar, AppBar, makeStyles } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import selectors from '../../../redux/auth/auth-selectors';
@@ -5,35 +6,51 @@ import { AuthNav } from '../AuthNav/AuthNav';
 import UserMenu from '../UserMenu/UserMenu';
 import s from './AppBar.module.css';
 
-function AppBar() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+
+  appBar: {
+    backgroundColor: '#424242',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}));
+
+function AppHeader() {
+  const classes = useStyles();
+
   const isAuth = useSelector(selectors.getIsAuthenticated);
   return (
     <>
-      <header className={s.header}>
-        <div>
-          <NavLink
-            to="/"
-            exact
-            className={s.navlink_main}
-            activeClassName={s.activenavlink}
-          >
-            Main
-          </NavLink>
-          {isAuth && (
+      <AppBar position="static">
+        <Toolbar className={classes.appBar}>
+          <div>
             <NavLink
-              to="/contacts"
+              to="/"
               exact
-              className={s.navlink_contacts}
+              className={s.navlink_main}
               activeClassName={s.activenavlink}
             >
-              Contacts
+              Main
             </NavLink>
-          )}
-        </div>
-        {isAuth ? <UserMenu /> : <AuthNav />}
-      </header>
+            {isAuth && (
+              <NavLink
+                to="/contacts"
+                exact
+                className={s.navlink_contacts}
+                activeClassName={s.activenavlink}
+              >
+                Contacts
+              </NavLink>
+            )}
+          </div>
+          {isAuth ? <UserMenu /> : <AuthNav />}
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
 
-export default AppBar;
+export default AppHeader;
